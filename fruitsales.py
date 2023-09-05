45074ca6c79401f305a29bc3251f3237d35a47b2
metrictulip0309 <eclairez995@gmail.com>
2023-09-04 21:43:49 -0400

    fruit sales

--- fruitsales.py
+++ fruitsales.py
@@ -1,3 +1,11 @@
-fruit_sales = pd.DataFrame({'Apples':[35,41],'Bananas':[21,34]},index=['2017 Sales','2018 Sales'])
-fruit_sales.to_csv('fruit.csv')
 # add your code here
+
+import pandas as pd
+
+
+fruit_sales = pd.DataFrame({'Apples':[35,41],'Bananas':[21,34]},index=['2017 Sales','2018 Sales'])
+
+fs = pd.DataFrame(fruit_sales, index = ['2017 Sales', '2018 Sales'])
+fs.to_csv('fruit.csv')
+
+print(fs)
